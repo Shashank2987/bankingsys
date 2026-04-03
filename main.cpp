1b6a178f1e33c2a10f870542fcfd6d842c619815
Shashank <rokshashank@gmail.com>
2026-04-03 21:31:02 +0530

    sql connection bugs

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
+#include <mysqlx/xdevapi.h>
 #include <iostream>
-#include <string>
-#include <vector>
-#include <sstream>
-#include <typeinfo>
+
 using namespace std;
-int main()
-{
-    cout<<"INTC"<<endl;
+
+int main() {
+    try {
+        mysqlx::Session session("localhost", 33060, "root", "your_password");
+
+        cout << "Connected successfully!" << endl;
+
+    } catch (const mysqlx::Error &err) {
+        cout << "Error: " << err.what() << endl;
+    }
+
+    return 0;
 }
