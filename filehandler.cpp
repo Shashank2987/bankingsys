b11ca66d7588db948017e3cd8005447325fcfe19
Shashank <rokshashank@gmail.com>
2026-04-04 22:45:22 +0530

    filehandler file bugged

---
+++ filehandler.cpp
@@ -0,0 +1,59 @@
+#include "filehandler.h"
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+using namespace std;
+vector<Account> load_accounts() {
+    vector<Account> accounts;
+    ifstream file("accounts.csv");
+
+    string line;
+
+    // if file doesn't exist, return empty list
+    if (!file.is_open()) {
+        return accounts;
+    }
+
+    // skip header
+    getline(file, line);
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string acc, name, pass, bal;
+
+        getline(ss, acc, ',');
+        getline(ss, name, ',');
+        getline(ss, pass, ',');
+        getline(ss, bal, ',');
+
+        // avoid empty lines crash
+        if (acc.empty()) continue;
+
+        accounts.push_back(Account(
+            stoi(acc),
+            name,
+            pass,
+            stod(bal)
+        ));
+    }
+
+    file.close();
+    return accounts;
+}
+
+void save_accounts(vector<Account> &accounts) {
+    ofstream file("accounts.csv");
+
+    // header
+    file << "account_no,name,password,balance\n";
+
+    for (auto &acc : accounts) {
+        file << acc.get_account_no() << ","
+             << acc.get_name() << ","
+             << acc.get_password() << ","
+             << acc.get_balance() << "\n";
+    }
+
+    file.close();
+}
